1b7189de3d86f13f0feac7e424cbec268760eecf
huangxuanping-weiyulai <1753901658@qq.com>
2020-01-15 19:35:15 +0800

    01.15 寒假作业

---
+++ 寒假作业/作业-Day 01/js基础题/02.js
@@ -0,0 +1,6 @@
+//使用一个var同时定义：变量名为age变量值为18；变量名为name变量值无；变量名为num变量值为NaN
+
+var age = 18;
+var name = "";
+var num = NaN ;
+console.log(num)
---
+++ 寒假作业/作业-Day 01/js基础题/04.js
@@ -0,0 +1,8 @@
+//指出下面代码的错误及原因，并给出你认为正确的修正
+
+// var num, age, name;
+var num1;
+var name = "杨";
+var age1 = 3;
+age = age1 = 18;
+console.log(age);
